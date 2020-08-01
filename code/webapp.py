8b772fc0341004c2f0e4fdd8c72b0e13f4de26fa
sharas <apkrovos@gmail.com>
2020-08-01 03:47:18 +0000

    moved http status setting out of try block in web app lookup handler

--- code/webapp.py
+++ code/webapp.py
@@ -8,12 +8,12 @@ class TzLookupResource(object):
     def on_get(self, req, resp):
         coord = (req.params.get(self.key_lat), req.params.get(self.key_lng))
         if not all(coord):
-            raise falcon.HTTPBadRequest("lat and lng parameters required for lookup")
+            raise falcon.HTTPBadRequest("lat and lng parameters required for timezone lookup")
         try:
-            resp.status = falcon.HTTP_200
             resp.body = tzlookup.findTZ(GeoCoord(*coord))
         except ValueError as e:
             raise falcon.HTTPBadRequest(str(e))
+        resp.status = falcon.HTTP_200
 
 app = falcon.API(media_type="text/plain;charset=UTF-8")
 app.add_route("/tz", TzLookupResource())
